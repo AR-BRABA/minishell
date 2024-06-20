988dbdd79bb75fd6645040dafe3526874454e2df
thatimoreira <thatimoreira1@gmail.com>
2024-06-20 09:19:20 -0300

    Add initial header file

---
+++ includes/minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/18 08:11:33 by tsoares-          #+#    #+#             */
+/*   Updated: 2024/06/18 08:58:12 by tsoares-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+
+#endif
+
