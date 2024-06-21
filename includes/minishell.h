cb4ca582eb225175cde7856b4926cc6d4c7d1635
thatimoreira <thatimoreira.dev@gmail.com>
2024-06-21 08:55:31 -0300

    Add read_input() function declaration. read_input() displays minishell prompt, read the user input and save the commands history.

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 08:11:33 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/06/18 08:58:12 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/06/21 08:55:18 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-
+void	read_input(char **user_input);
 
 #endif
 
