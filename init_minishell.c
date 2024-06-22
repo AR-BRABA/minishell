73ae45cb358d523c332415a2efe89ca05b9f3ce6
thatimoreira <thatimoreira.dev@gmail.com>
2024-06-22 03:37:46 -0300

    Implement validate_input() function call.

--- init_minishell.c
+++ init_minishell.c
@@ -6,11 +6,16 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 05:36:05 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/06/21 12:14:40 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/06/22 01:00:44 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
+
+/**
+ * Main function that reads the user input in a loop and validates the input
+ * @return 0 on successful execution
+ */
 
 int	main(int argc, char **argv)
 {
@@ -19,6 +24,8 @@ int	main(int argc, char **argv)
 		read_input(argv);
 		if (!argv)
 			break ;
+		if (validate_input(*argv))
+			printf("Valid input: %s\n", *argv);
 	}
 	(void)argc;
 	return (0);
