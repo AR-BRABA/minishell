45d96f64c6f6a4f9bc301927fe0e0c5511bcdb61
thatimoreira <thatimoreira.dev@gmail.com>
2024-06-22 03:36:16 -0300

    Add validate_input() function prototyte.

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,20 +6,21 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 08:11:33 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/06/21 10:42:02 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/06/22 00:59:39 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 void	read_input(char **user_input);
+bool	validate_input(char *input);
 
 #endif
-
