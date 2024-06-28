0855f5dfbb852b636ad8f6ceeb50ecf4b6112476
thatimoreira <thatimoreira.dev@gmail.com>
2024-06-28 16:14:11 -0300

    Include libft auxiliary library path.

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 08:11:33 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/06/24 21:27:37 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/06/28 15:40:17 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <stdbool.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include "libft/libft.h"
 
 char	*read_input(char *user_input);
 bool	validate_input(char *input);
