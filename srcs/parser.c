fea807eb783881d23593d3e980bd73de93f6d19e
jou <jou.42.rio@gmail.com>
2024-06-29 13:07:15 -0300

    add parser.c with libft utils and main

---
+++ srcs/parser.c
@@ -0,0 +1,115 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+	i = 0;
+
+	while (str && str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strtrim(char *s1)
+{
+	char	*trim;
+	int		start;
+	int		end;
+	int		i;
+
+	i = 0;
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (s1[start] && s1[start] == ' ')
+		start++;
+	while (end > start && s1[end] == ' ')
+		end--;
+	trim = (char *) malloc((end - start + 2) * sizeof(char));
+	if (!trim)
+		return (NULL);
+	while (start <= end)
+	{
+		trim[i] = s1[start];
+		i++;
+		start++;
+	}
+	trim[i] = '\0';
+	free(s1);
+	return (trim);
+}
+
+char	*trim_start(char *s1)
+{
+	char	*trim;
+	int		start;
+	int		end;
+	int		i;
+
+	i = 0;
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (s1[start] && s1[start] == ' ')
+		start++;
+	trim = (char *) malloc((end - start + 2) * sizeof(char));
+	if (!trim)
+		return (NULL);
+	while (start <= end)
+	{
+		trim[i] = s1[start];
+		i++;
+		start++;
+	}
+	trim[i] = '\0';
+	free(s1);
+	return (trim);
+}
+
+char	*ft_substr(char *s, unsigned int start, int len)
+{
+	int	i;
+	char	*substr;
+
+	i = 0;
+	substr = malloc ((len + 1) * sizeof(char));
+	if (!substr)
+		return (NULL);
+	while (start < ft_strlen(s) && i < len)
+	{
+		substr[i] = s[start];
+		i++;
+		start++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+
+char	**lexer(char *user_input)
+{
+	int	i = 0;
+	char	**tokens;
+	
+	user_input = ft_strtrim(user_input);
+	while (user_input && user_input[i] != '\0')
+	{
+
+	}
+
+	return (tokens);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i = 0;
+	char	**tokens;
+
+	if (argc < 2)
+		return (1);
+	tokens = lexer(argv[1]);
+	while (tokens[i] != NULL)
+	{
+		printf("%d: %s\n", i, tokens[i]);
+		i++;
+	}
+	return (0);
+}
