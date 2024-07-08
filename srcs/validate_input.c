b60ddcb7c344f6b2a367668b3d44ba2322be0cc9
thatimoreira <thatimoreira.dev@gmail.com>
2024-07-08 18:50:00 -0300

    Modified quotes validation logic to return the position of the next char after closed quotes or -1 when it's unclosed.

--- srcs/validate_input.c
+++ srcs/validate_input.c
@@ -13,6 +13,35 @@
 #include "../includes/minishell.h"
 
 /**
+ * Checks for unclosed quotes in user input
+ *
+ * @param input - user input
+ * @return -1 - if there are unclosed quotes, otherwise returns the position of the char after the quotes close
+ */
+
+int	check_unclosed_quotes(char *input)
+{
+	bool	single_quote_open;
+	bool	double_quote_open;
+	int		pos;
+
+	single_quote_open = false;
+	double_quote_open = false;
+	pos = 0;
+	while (input[pos])
+	{
+		if (input[pos] == '\'' && !double_quote_open)
+			single_quote_open = !single_quote_open;
+		else if (input[pos] == '\"' && !single_quote_open)
+			double_quote_open = !double_quote_open;
+		pos++;
+	}
+	if (single_quote_open || double_quote_open)
+		return (-1);
+	return (pos);
+}
+
+/**
  * Checks if user input starts or ends with '|', '<' or '>'
  *
  * @param input - user input
@@ -30,31 +59,6 @@ bool	check_special_chars(char *input)
 }
 
 /**
- * Checks for unclosed quotes in user input
- *
- * @param input - user input
- * @return true - if there are unclosed quotes, false otherwise
- */
-
-bool	check_unclosed_quotes(char *input)
-{
-	bool	single_quote_open;
-	bool	double_quote_open;
-
-	single_quote_open = false;
-	double_quote_open = false;
-	while (*input)
-	{
-		if (*input == '\'' && !double_quote_open)
-			single_quote_open = !single_quote_open;
-		else if (*input == '\"' && !single_quote_open)
-			double_quote_open = !double_quote_open;
-		input++;
-	}
-	return (single_quote_open || double_quote_open);
-}
-
-/**
  * Validates user input by checking various conditions
  *
  * @param input (user input)
@@ -62,7 +66,7 @@ bool	check_unclosed_quotes(char *input)
  */
 bool	validate_input(char *input)
 {
-	if (check_unclosed_quotes(input))
+	if (check_unclosed_quotes(input) == -1)
 	{
 		fprintf(stderr, "Error: Unclosed quotes in input.\n");
 		return (false);
