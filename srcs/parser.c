10c7d08a2dc4b16cf6b4457989d8cad8e202c333
jou <jou.42.rio@gmail.com>
2024-07-09 17:54:53 -0300

    malloc done but a lot of problems

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h> // strcpy
 
 int	ft_strlen(char *str)
 {
@@ -35,7 +36,6 @@ char	*ft_strtrim(char *s1)
 		start++;
 	}
 	trim[i] = '\0';
-	free(s1);
 	return (trim);
 }
 
@@ -77,110 +77,104 @@ int	is_basic(char c)
 	return (!(is_special(c)) && !(is_quote(c)) && !(is_space(c)));
 }
 
-int	count_tokens(char *user_input)
-{
-	int	i = 0;
-	int	start = 0;
-	int	token = 0;
-
-	while (user_input && user_input[i] != '\0')
-	{
-		start = i;
-		if (is_special(user_input[i]))
-		{
-			i++;
-			if (is_special(user_input[i]) && user_input[i] == user_input[i - 1])
-			// se eh especial (<,|,>) e se o proximo eh igual (>>, <<)
-				i++;
-		}
-		else if (is_quote(user_input[i]))
-		{
-			i++;
-			while (user_input[i] != '\0' && user_input[i] != user_input[start])
-				i++;
-		}
-		else if (is_basic(user_input[i]))
-		{
-			i++;
-			while (user_input[i] != '\0' && is_basic(user_input[i]))
-				i++;
-		}
-		token++;
-		if (is_space(user_input[i]))
-			while (is_space(user_input[i]))
-				i++;
-	}
-	return (token);
-}
-
-void	tokenizer(char *user_input)
+int	count_tokens(char *input)
 {
 	int	count = 0;
 	int	start = 0;
-	// int	count = 0;
-	// char	**tokens;
+	int	tokens = 0;
+
+	while (input && input[count] != '\0')
+	{
+		start = count;
+		count++;
+		if (is_special(input[start]))
+		{
+			if (is_special(input[count]) && input[count] == input[start])
+				count++;
+		}
+		else if (is_quote(input[start]))
+		{
+			while (input[count] != '\0' && input[count] != input[start])
+				count++;
+			count++;
+		}
+		else if (is_basic(input[start]))
+		{
+			while (input[count] != '\0' && is_basic(input[count]))
+				count++;
+		}
+		tokens++;
+		if (is_space(input[count]))
+			while (is_space(input[count]))
+				count++;
+	}
+	return (tokens);
+}
+
+char	**tokenizer(char *user_input)
+{
+	int	count = 0;
+	int	start = 0;
+	int	token = 0;
+	char	**array;
+	char	*input;
 	
 	// expansao de variaveis? ver no futuro
 	// $
-	// tokens = (char **)malloc(count_tokens(user_input) * sizeof(char *));
-	// user_input = ft_strtrim(user_input);
-	while (user_input && user_input[count] != '\0')
+	input = ft_strtrim(user_input);
+	array = (char **)malloc(count_tokens(input) * sizeof(char *));
+	printf("\n\ntokens + null = %i\n\n\n", count_tokens(input));
+	while (input && input[count] != '\0')
 	{
 		start = count;
-		printf("%c", user_input[start]);
 		count++;
-		if (is_special(user_input[start]))
+		if (is_special(input[start]))
 		{
-			if (is_special(user_input[count]) && user_input[count] == user_input[start])
+			if (is_special(input[count]) && input[count] == input[start])
 			// se eh especial (<,|,>) e se o proximo eh igual (>>, <<)
 			{
-				printf("%c", user_input[count]);
 				count++;
 			}
 		}
-		else if (is_quote(user_input[start]))
+		else if (is_quote(input[start]))
 		{
-			while (user_input[count] != '\0' && user_input[count] != user_input[start])
+			while (input[count] != '\0' && input[count] != input[start])
 			{
-				printf("%c", user_input[count]);
 				count++;
 			}
-			printf("%c", user_input[count]);
 			count++;
 		}
-		else if (is_basic(user_input[start]))
+		else if (is_basic(input[start]))
 		{
-			while (user_input[count] != '\0' && is_basic(user_input[count]))
+			while (input[count] != '\0' && is_basic(input[count]))
 			{
-				printf("%c", user_input[count]);
 				count++;
 			}
 		}
-		// tokens[count++] = ft_substr(&user_input[start], (i - start));
-		if (is_space(user_input[count]))
-			while (is_space(user_input[count]))
+		array[token++] = ft_substr(&input[start], (count - start));
+		if (is_space(input[count]))
+			while (is_space(input[count]))
 				count++;
-		printf("\n");
 	}
-	// tokens[count] = NULL;
-	// falta malloc
-	// return (tokens);
+	array[token] = NULL;
+	free(input);
+	return (array);
 }
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	// int	i = 0;
-	// char	**tokens;
+	int	i = 0;
+	char	*input;
+	char	**tokens;
 	
-	(void)argv;	
-	if (argc < 2)
-		return (1);
-	tokenizer("teste tokens             aqui 'e 'mais uma'aqui'     << >> |<< |");
-	// tokens = tokenizer(argv[1]);
-	// while (tokens[i] != NULL)
-	// {
-	// 	printf("%d: %s\n", i, tokens[i]);
-	// 	i++;
-	// }
+	input = (char *)malloc(69 * sizeof(char));
+	input = strcpy(input, "teste tokens             aqui 'e 'mais uma'aqui'         << >> |<< |");
+	tokens = tokenizer(input);
+	while (tokens[i] != NULL)
+	{
+		printf("%d: <%s>\n", i, tokens[i]);
+		i++;
+	}
+	free(input);
 	return (0);
 }
