16bed2e70035ec03a25e36b201740e07d798c5d9
thatimoreira <thatimoreira.dev@gmail.com>
2024-07-10 18:54:17 -0300

    Fix bug that wasn't validating three '<' in sequence as an invalid input.

--- srcs/validate_input.c
+++ srcs/validate_input.c
@@ -18,7 +18,6 @@
  * @param input - user input
  * @return -1 - if there are unclosed quotes, otherwise returns the position of the char after the quotes close
  */
-
 int	check_unclosed_quotes(char *input)
 {
 	bool	single_quote_open;
@@ -40,6 +39,50 @@ int	check_unclosed_quotes(char *input)
 		return (-1);
 	return (pos);
 }
+/*
+bool	check_redirection_syntax(char *input)
+{
+	char	*redirections;
+
+	redirections = "<>";
+	// check_uclosed_quotes
+	while (*input)
+	{
+		if (ft_strchr(redirections, *input))
+		{
+			input++;
+			while (*input && ft_isspace(*input))
+				input++;
+			// check_unclosed_quotes
+		}
+	}
+}*/
+
+/**
+ * Checks if user input starts or ends with '|', '<' or '>'
+ *
+ * @param input - user input
+ * @return true - if input starts or ends with '|', '<' or '>', false otherwise
+ */
+bool	check_border_special_chars(char *input)
+{
+	char	*special_chars;
+
+	special_chars = "|<>";
+	if (*input == '<' && *(input + 1) == '<')
+	{
+		input += 2;
+		while (*input && ft_isspace(*input) && *input != '<')
+			input++;
+		if (*input == '<')
+			return (true);
+		return (false);
+	}
+	if (ft_strchr(special_chars, input[0])
+		|| ft_strchr(special_chars, input[ft_strlen(input) - 1]))
+		return (true);
+	return (false);
+}
 
 /**
  * Checks if user input is an empty string or contains only spaces
@@ -55,24 +98,8 @@ bool	check_empty_input(char *input)
 			return (false);
 		input++;
 	}
+	// >>>>>>>>>>> Give the prompt back to user
 	return (true);
-}
-
-/**
- * Checks if user input starts or ends with '|', '<' or '>'
- *
- * @param input - user input
- * @return false - if input starts or ends with '|', '<' or '>', true otherwise
- */
-bool	check_special_chars(char *input)
-{
-	char	*special_chars;
-
-	special_chars = "|<>";
-	if (ft_strchr(special_chars, input[0])
-		|| ft_strchr(special_chars, input[ft_strlen(input) - 1]))
-		return (true);
-	return (false);
 }
 
 /**
@@ -85,7 +112,7 @@ bool	validate_input(char *input)
 {
 	if (check_empty_input(input))
 	{
-		fprintf(stderr, "DEVOLVER PROMPT PARA USUÁRIO.\n");
+		fprintf(stderr, "GIVE THE PROMPT BACK TO USER.\n");
 		return (false);
 	}
 	if (check_unclosed_quotes(input) == -1)
@@ -93,10 +120,10 @@ bool	validate_input(char *input)
 		fprintf(stderr, "Error: Unclosed quotes in input.\n");
 		return (false);
 	}
-	if (check_special_chars(input))
+	if (check_border_special_chars(input))
 	{
 		fprintf(stderr, "Error: Special chars at the beginning or"
-			"end of the input.\n");
+			" end of the input.\n");
 		return (false);
 	}
 	return (true);
