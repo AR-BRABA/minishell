18d01d78776eccda92f2c74f1693c094229d2f32
thatimoreira <thatimoreira1@gmail.com>
2024-08-18 04:25:36 -0300

    Align test results table and add color-coded status for readability
    - Ensure proper alignment of the test results table using formatted printf
    - Add color coding for the status column: green for PASSED and red for FAILED

--- tests/scripts/border_special_chars.sh
+++ tests/scripts/border_special_chars.sh
@@ -31,24 +31,76 @@ run_test() {
 
     # Compare the actual filtered output with the expected output
     if diff "$result_file" "$expected_file" > /dev/null; then
-        results+=(" $test_id  |   $input   |   PASSED")
+        #results+=(" $test_id  |   $input   |   PASSED")
+        results+=("$(printf "%-4s | %-15s | ${GREEN}%-7s${NC}" "$test_id" "$input" "PASSED")")
     else
-        results+=(" $test_id  |   $input   |   FAILED")
+        #results+=(" $test_id  |   $input   |   FAILED")
+        results+=("$(printf "%-4s | %-15s | ${RED}%-7s${NC}" "$test_id" "$input" "FAILED")")
     fi
 }
 
 # Test cases
-run_test '\command' 'minishell$ \\command\nError: Special chars at the beginning or end of the input.' 1
-run_test 'command|' 'minishell$ command|\nError: Special chars at the beginning or end of the input.' 2
-run_test '<command' 'minishell$ <command\nError: Special chars at the beginning or end of the input.' 3
-run_test 'command<' 'minishell$ command<\nError: Special chars at the beginning or end of the input.' 4
-run_test '>command' 'minishell$ >command\nError: Special chars at the beginning or end of the input.' 5
-run_test 'command>' 'minishell$ command>\nError: Special chars at the beginning or end of the input.' 6
-run_test 'command>>' 'minishell$ command>>\nError: Special chars at the beginning or end of the input.' 7
+run_test '\command' 'minishell$ \\command\nError' 1
+run_test 'command|' 'minishell$ command|\nError' 2
+run_test '<command' 'minishell$ <command\nError' 3
+run_test 'command<' 'minishell$ command<\nError' 4
+run_test '>command' 'minishell$ >command\nError' 5
+run_test 'command>' 'minishell$ command>\nError' 6
+run_test 'command>>' 'minishell$ command>>\nError' 7
+run_test '<<' 'minishell$ <<\nValid input: <<' 8
+run_test '<< anything' 'minishell$ << anything\nValid input: <<' 9
+run_test '<<anything' 'minishell$ <<anything\nValid input: <<' 10
+run_test '|' 'minishell$ |\nError' 11
+run_test '|anything' 'minishell$ |anything\nError' 12
+run_test '| anything' 'minishell$ | anything\nError' 13
+run_test 'anything|' 'minishell$ anything|\nError' 14
+run_test 'anything |' 'minishell$ anything |\nError' 15
+run_test '<' 'minishell$ <\nError' 16
+run_test '< anything' 'minishell$ < anything\nError' 17
+run_test 'anything <' 'minishell$ anything <\nError' 18
+run_test '<|' 'minishell$ <|\nError' 19
+run_test 'anything <|' 'minishell$ anything <|\nError' 20
+run_test '<| anything' 'minishell$ <| anything\nError' 21
+run_test '< |' 'minishell$ < |\nError' 22
+run_test 'anything < |' 'minishell$ anything < |\nError' 23
+run_test '< | anything' 'minishell$ < | anything\nError' 24
+run_test '<<<' 'minishell$ <<<\nError' 25
+run_test 'anything <<<' 'minishell$ anything <<<\nError' 26
+run_test '<<< anything' 'minishell$ <<< anything\nError' 27
+run_test '< <<' 'minishell$ < <<\nError' 28
+run_test 'anything < <<' 'minishell$ anything < <<\nError' 29
+run_test '< << anything' 'minishell$ < << anything\nError' 30
+run_test '<< <' 'minishell$ << <\nError' 31
+run_test 'anything << <' 'minishell$ anything << <\nError' 32
+run_test '<< < anything' 'minishell$ << < anything\nError' 33
+run_test '< < <' 'minishell$ < < <\nError' 34
+run_test 'anything < < <' 'minishell$ anything < < <\nError' 35
+run_test '< < < anything' 'minishell$ < < < anything\nError' 36
+run_test '>' 'minishell$ >\nError' 37
+run_test '> anything' 'minishell$ > anything\nError' 38
+run_test 'anything >' 'minishell$ anything >\nError' 39
+run_test '>|' 'minishell$ >|\nError' 40
+run_test 'anything >|' 'minishell$ anything >|\nError' 41
+run_test '>| anything' 'minishell$ >| anything\nError' 42
+run_test '> |' 'minishell$ > |\nError' 43
+run_test 'anything > |' 'minishell$ anything > |\nError' 44
+run_test '> | anything' 'minishell$ > | anything\nError' 45
+run_test '>>>' 'minishell$ >>>\nError' 46
+run_test 'anything >>>' 'minishell$ anything >>>\nError' 47
+run_test '>>> anything' 'minishell$ >>> anything\nError' 48
+run_test '> >>' 'minishell$ > >>\nError' 49
+run_test 'anything > >>' 'minishell$ anything > >>\nError' 50
+run_test '> >> anything' 'minishell$ > >> anything\nError' 51
+run_test '>> >' 'minishell$ >> >\nError' 52
+run_test 'anything >> >' 'minishell$ anything >> >\nError' 53
+run_test '>> > anything' 'minishell$ >> > anything\nError' 54
+run_test '> > >' 'minishell$ > > >\nError' 55
+run_test 'anything > > >' 'minishell$ anything > > >\nError' 56
+run_test '> > > anything' 'minishell$ > > > anything\nError' 57
 
 # Print results table
 printf "\nTEST: %s" "$TEST_NAME"
-printf "\n#id |     TESTS    |   Status\n"
+printf "\n%-4s | %-15s | %s\n" "#id" "TESTS" "Status"
 printf -- "%s\n" "${results[@]}"
 
 # Cleanup - Uncomment to remove test outputs after running
