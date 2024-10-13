25cf5f675782ec33c4aab14d1348c897ec6ca482
thatimoreira <thatimoreira1@gmail.com>
2024-10-13 18:14:32 -0300

    Remove comment that call the built-ins validation

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 15:42:28 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/10/02 16:17:09 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/10/13 18:09:52 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,11 @@ void	execute_commands(t_tab *cmdtable)
 	while (cmdlist != NULL)
 	{
 		token = cmdlist->head; // 1º token do user_input é o comando
-		execute_external_command(token);
-		/*if (!execute_builtins(token))
+		if (execute_builtins(token) == -1)
 		{
-			printf("CHAMANDO EXECUTE_EXTERNAL_COMMANDS");
+			printf("CHAMANDO EXECUTE_EXTERNAL_COMMANDS\n");
 			execute_external_command(token);
-		}	*/	
+		}	
 		// Se tiver mais linhas de comando, ir p/a próxima
 		cmdlist = cmdlist->next;
 	}
