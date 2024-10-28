cd68f11b73ad0cc4232e9ba2c98663c67b993cdc
jou <jou.42.rio@gmail.com>
2024-10-28 18:59:44 -0300

    adds \n on pwd

--- srcs/pwd/pwd.c
+++ srcs/pwd/pwd.c
@@ -7,6 +7,7 @@ int	pwd(void)
 	buf = NULL;
 	buf = getcwd(buf, 0);
 	ft_putstr_fd(buf, 1); // substituir 1 por STDOUT
+	ft_putstr_fd("\n", 1); // substituir 1 por STDOUT
 	if (!buf)
 		return (1);
 	free(buf);
