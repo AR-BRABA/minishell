7ca4b554947a036f854d364365e71e0a4b99d954
jou <jou.42.rio@gmail.com>
2025-01-29 10:15:11 -0300

    cd.c refact

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -25,33 +25,41 @@ void	update_key_value(t_env *list, char *key, char *new_val)
 	}
 }
 
+static char *get_cd_path(char **args, t_env *env)
+{
+    char *path;
+
+    if (!args || ft_strncmp(args[0], "~", 1) == 0)
+    {
+        path = get_key_value(env, "HOME");
+        if (args && args[0][1] != '\0')
+            path = ft_strfjoin(path, &args[0][1]);
+    }
+    else if (ft_strncmp(args[0], "-", 2) == 0)
+        path = get_key_value(env, "OLDPWD");
+    else
+        path = ft_strdup(args[0]);
+    return path;
+}
+
 int	ft_cd(char **args, t_env *env)
 {
-	char	*origin;
-	char	*path;
-	char	buf[PATH_MAX + 1];
+    char	*origin;
+    char	*path;
+    char	buf[PATH_MAX + 1];
 
-	if (split_len(args) > 1)
-		return (perror("cd:"), 1);
-	if (!args || ft_strncmp(args[0], "~", 1) == 0)
-	{
-		path = get_key_value(env, "HOME");
-		if (args && args[0][1] != '\0')
-			path = ft_strfjoin(path, &args[0][1]);	
-	}
-	else if (ft_strncmp(args[0], "-", 2) == 0)
-		path = get_key_value(env, "OLDPWD");
-	else
-		path = ft_strdup(args[0]);
-	origin = getcwd(buf, PATH_MAX + 1);
-	if (chdir(path) == -1 || !origin)
-	{
-		perror("cd:");
-		free(path);
-		return (1);
-	}
-	update_key_value(env, "OLDPWD", origin);
-	update_key_value(env, "PWD", path);
-	free(path);
-	return (0);
+    if (split_len(args) > 1)
+        return (perror("cd:"), 1);
+    path = get_cd_path(args, env);
+    origin = getcwd(buf, PATH_MAX + 1);
+    if (chdir(path) == -1 || !origin)
+    {
+        perror("cd:");
+        free(path);
+        return (1);
+    }
+    update_key_value(env, "OLDPWD", origin);
+    update_key_value(env, "PWD", path);
+    free(path);
+    return (0);
 }
