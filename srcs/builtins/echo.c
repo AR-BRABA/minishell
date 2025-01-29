f64066ddebb6d30f1781291e9181726499480104
jou <jou.42.rio@gmail.com>
2025-01-29 10:19:56 -0300

    echo.c refact

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -1,28 +1,34 @@
 #include "../../includes/minishell.h"
 
+int	check_n_flag(char **arg)
+{
+    int	n = 0;
+
+    if (arg && arg[0] && ft_strncmp(arg[0], "-n", 2) == 0)
+    {
+        n = 2;
+        while (arg[0][n] == 'n')
+            n++;
+        if (n != (int)ft_strlen(arg[0]))
+            n = 0;
+        else
+            n = 1;
+    }
+    return (n);
+}
+
 int	ft_echo(char **arg)
 {
-	int	n = 0;
-	int	i;
+    int	n = check_n_flag(arg);
+    int	i = n;
 
-	if (arg && arg[0] && ft_strncmp(arg[0], "-n", 2) == 0)
-	{
-		n = 2;
-		while (arg[0][n] == 'n')
-			n++;
-		if (n != (int)ft_strlen(arg[0]))
-			n = 0;
-		else
-			n = 1;
-	}
-	i = n;
-	while (arg && arg[i])
-	{
-		ft_putstr_fd(arg[i++], 1);
-		if (arg[i])
-			ft_putchar_fd(' ', 1);
-	}
-	if (!n)
-		ft_putchar_fd('\n', 1);
-	return (0);
+    while (arg && arg[i])
+    {
+        ft_putstr_fd(arg[i++], 1);
+        if (arg[i])
+            ft_putchar_fd(' ', 1);
+    }
+    if (!n)
+        ft_putchar_fd('\n', 1);
+    return (0);
 }
