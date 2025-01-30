3cbe06b5d5bc9adbb456f6ec1656410acec5725b
jou <jou.42.rio@gmail.com>
2025-01-30 08:48:14 -0300

    export.c

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -15,32 +15,40 @@ void	update_envnode(char *value, t_envnode *node)
 	node->value = ft_strdup(value);
 }
 
+static int	handle_export_arg(char *arg, t_env *envp)
+{
+    t_envnode	*new = new_envnode(arg);
+    t_envnode	*old;
+
+    if (!new)
+        return (1);
+    old = search_key(envp, new->key);
+    if (!old && str_isname(new->key) && !ft_isdigit(new->key[0]))
+        addback_env(new, envp);
+    else
+    {
+        if (old)
+            update_envnode(new->value, old);
+        else
+        {
+            ft_putstr_fd("minishell: export: not a valid identifier\n", 2);
+            free_envnode(new);
+            return (1);
+        }
+        free_envnode(new);
+    }
+    return (0);
+}
+
 int	ft_export(char **args, t_env *envp)
 {
-	int	ret = 0;
-	int	count = 0;
-	t_envnode	*new;
-	t_envnode	*old;
-	
-	while (args && args[count] != NULL)
-	{
-		new = new_envnode(args[count++]);
-		if (!new)
-			return (1);
-		old = search_key(envp, new->key);
-		if (!old && str_isname(new->key) && !ft_isdigit(new->key[0]))
-			addback_env(new, envp);
-		else
-		{
-			if (old)
-				update_envnode(new->value, old);
-			else
-			{
-				perror("export:");
-				ret = 1;
-			}
-			free_envnode(new);
-		}
-	}
-	return(ret);
+    int	ret = 0;
+    int	count = 0;
+
+    while (args && args[count] != NULL)
+    {
+        if (handle_export_arg(args[count++], envp))
+            ret = 1;
+    }
+    return (ret);
 }
