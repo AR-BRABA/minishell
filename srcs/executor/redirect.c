013d76af7f3691b9b535f1d3d221edc2effe3f49
jou <jou.42.rio@gmail.com>
2025-02-02 21:55:42 -0300

    refact redirect

--- srcs/executor/redirect.c
+++ srcs/executor/redirect.c
@@ -1,65 +1,83 @@
 #include "../../includes/minishell.h"
-#include <fcntl.h>
+
+int do_redirect_out(t_node *token, int *fd)
+{
+	if (token->type == APPEND)
+		fd[1] = open(token->next->value, O_CREAT | O_APPEND | O_RDWR, 0664);
+	else
+		fd[1] = open(token->next->value, O_CREAT | O_TRUNC | O_RDWR, 0664);
+	if (fd[1] < 0 || dup2(fd[1], 1) < 0)
+	{
+		perror(token->next->value);
+		return (1);
+	}
+	close(fd[1]);
+	return (0);
+}
+
+int do_heredoc(t_node *token, int *fd)
+{
+	char *input;
+
+	if (pipe(fd) < 0)
+	{
+		perror(token->next->value);
+		return (1);
+	}
+	input = readline("> ");
+	while (ft_strncmp(input, token->next->value, ft_strlen(token->next->value) + 1) != 0) 
+	{
+		write(fd[1], input, ft_strlen(input));
+		write(fd[1], "\n", 1);
+		free(input);
+		input = readline("> ");
+	}
+	if (dup2(fd[0], 0) < 0)
+	{
+		perror(token->next->value);
+		return (1);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	return (0);
+}
+
+int do_redirect_in(t_node *token, int *fd)
+{
+	fd[0] = open(token->next->value, O_RDONLY);
+	if (fd[0] < 0 || dup2(fd[0], 0) < 0)
+	{
+		perror(token->next->value);
+		return (1);
+	}
+	close(fd[0]);
+	token = token->next->next;
+	return (0);
+}
 
 int	redirect(t_list *cmdlist)
 {
 	int	fd[2];
-	t_node *token= cmdlist->head;
-	char *input;
+	t_node *token;
 
+	token = cmdlist->head;
 	while (token != NULL && token->next != NULL)
 	{
 		if (token->type == REDIRECT_IN)
 		{
-			fd[0] = open(token->next->value, O_RDONLY);
-			if (fd[0] < 0 || dup2(fd[0], 0) < 0)
-			{
-				perror(token->next->value);
+			if (do_redirect_in(token, fd) == 1)
 				return (1);
-			}
-			close(fd[0]);
-			token = token->next->next;
 		}
 		else if (token->type == HEREDOC)
 		{
-			if (pipe(fd) < 0)
-			{
-				perror(token->next->value);
+			if (do_heredoc(token, fd) == 1)
 				return (1);
-			}
-			input = readline("> ");
-			while (ft_strncmp(input, token->next->value, ft_strlen(token->next->value) + 1) != 0) 
-			{
-				write(fd[1], input, ft_strlen(input));
-				write(fd[1], "\n", 1);
-				free(input);
-				input = readline("> ");
-			}
-			if (dup2(fd[0], 0) < 0)
-			{
-				perror(token->next->value);
-				return (1);
-			}
-			close(fd[0]);
-			close(fd[1]);
-			token = token->next->next;
 		}
 		else if (token->type == REDIRECT_OUT || token->type == APPEND)
-		{
-			if (token->type == APPEND)
-				fd[1] = open(token->next->value, O_CREAT | O_APPEND | O_RDWR, 0664);
-			else
-				fd[1] = open(token->next->value, O_CREAT | O_TRUNC | O_RDWR, 0664);
-			if (fd[1] < 0 || dup2(fd[1], 1) < 0)
-			{
-				perror(token->next->value);
+			if (do_redirect_out(token, fd) == 1)
 				return (1);
-			}
-			close(fd[1]);
-			token = token->next->next;
-		}
-		else
-			token = token->next;
+		token = token->next;
 	}
 	return (0);
 }
+
