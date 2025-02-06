6c4e357477b04ab148c4b7a86fd40e2772b19129
thatimoreira <thatimoreira.dev@gmail.com>
2025-02-06 17:28:42 -0300

    Fix quote validation: allow nested unclosed quotes inside closed ones

--- srcs/input_validation/validate_input_utils.c
+++ srcs/input_validation/validate_input_utils.c
@@ -23,24 +23,28 @@ void	skip_spaces(char **input)
 		(*input)++;
 }
 
-bool	check_double_quotes(char *s, bool unclosed_double_quotes)
+bool	check_double_quotes(char **s)
 {
-	while (*s && *s != '\"')
-		s++;
-	if (*s == '\"')
-		unclosed_double_quotes = !unclosed_double_quotes;
-	check_unclosed_quotes(s + 1);
-	return (unclosed_double_quotes);
+	while (**s && **s != '\"')
+		(*s)++;
+	if (**s == '\"')
+	{
+		(*s)++;
+		return (false);
+	}
+	return (true);
 }
 
-bool	check_single_quotes(char *s, bool unclosed_single_quotes)
+bool	check_single_quotes(char **s)
 {
-	while (*s && *s != '\'')
-		s++;
-	if (*s == '\'')
-		unclosed_single_quotes = !unclosed_single_quotes;
-	check_unclosed_quotes(s + 1);
-	return (unclosed_single_quotes);
+	while (**s && **s != '\'')
+		(*s)++;
+	if (**s == '\'')
+	{
+		(*s)++;
+		return (false);
+	}
+	return (true);
 }
 
 bool	check_unclosed_quotes(char *s)
@@ -53,12 +57,20 @@ bool	check_unclosed_quotes(char *s)
 	while (*s)
 	{
 		if (*s == '\"')
+		{
+			s++;
 			unclosed_double_quotes =
-				check_double_quotes(s, unclosed_double_quotes);
+				check_double_quotes(&s);
+		}
 		else if (*s == '\'')
-				unclosed_single_quotes =
-					check_single_quotes(s, unclosed_single_quotes);
-		s++;
+		{
+			s++;
+			unclosed_single_quotes =
+				check_single_quotes(&s);
+		}
+		else
+			s++;
 	}
 	return (unclosed_single_quotes || unclosed_double_quotes);
 }
+// For each function => Syntax error: <error type> + exit_status = 2
